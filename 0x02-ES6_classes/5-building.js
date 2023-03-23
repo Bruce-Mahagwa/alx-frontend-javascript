9af8c24238335c8f3cc21e3a6f36d292f1cdfe9c
Bruce-Mahagwa <jacobbruce880@gmail.com>
2023-03-23 07:15:38 -0700

    Defines a parent class with a method that must be passed down to its children

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor.name !== 'Building'
+        && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  set sqft(feet) {
+    this._sqft = feet;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
