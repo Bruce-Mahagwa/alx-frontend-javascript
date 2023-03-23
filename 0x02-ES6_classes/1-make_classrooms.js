9af5c2eb4e82ce2b90079323660f6598983fc447
Bruce-Mahagwa <jacobbruce880@gmail.com>
2023-03-23 03:18:02 -0700

    Instantiates a class 3 times and returns an array of the objects

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,8 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom';
+
 export default function initializeRooms() {
-  let obj = new ClassRoom(19);
-  let obj2 = new ClassRoom(20);
-  let obj3 = new ClassRoom(34);
+  const obj = new ClassRoom(19);
+  const obj2 = new ClassRoom(20);
+  const obj3 = new ClassRoom(34);
   return [obj, obj2, obj3];
 }
